8017f5a0bf11ecbf949657b60d3c253a40700f52
cprichmond <cprichmond@hotmail.com>
2017-03-22 08:13:07 +1100

    Started to add real images

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react';
 import Slider from 'react-slick';
 import logo from './logo.svg';
 import './App.css';
+import leftArrow from './img/arrowL.png';
 
+class LeftNavButton extends Component {
+  render() {
+    return <button className="bs-left-arrow" onClick={this.props.onClick}><img src={leftArrow} alt="Left Arrow" /></button>
+  }
+}
+
+class RightNavButton extends Component {
+  render() {
+    return <button onClick={this.props.onClick}>Next</button>
+  }
+}
 
 class ReactSlickDemo extends Component {
   render() {
   	let settings = {
-    	dots: true
+    	dots: true,
+      slidesToShow: 6,
+      responsive: [ { breakpoint: 768, settings: { slidesToShow: 3 } }, { breakpoint: 1024, settings: { slidesToShow: 5 } }, { breakpoint: 100000, settings: { slidesToShow: 6 } } ],
+      nextArrow: <RightNavButton />,
+      prevArrow: <LeftNavButton />
     };
     return (
-    	<div className='container'>
+    	<div className="container">
       	<Slider {...settings}>
         	<div><img alt="" src='http://placekitten.com/g/400/200' /></div>
           <div><img alt="" src='http://placekitten.com/g/400/200' /></div>
